fd7073088b111de705603e56ce97e952492c8913
Anatoly Popov <popov.anatoly@gmail.com>
2018-08-19 11:38:02 +0300

    Added Manufaturer model and migratons

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=150)
-    manufacturer = models.CharField(max_length=70)
+    manufacturer = models.ManyToManyField("Manufacturer", related_name="products")
     size = models.IntegerField(default=0)
     date_added = models.DateTimeField(blank=True, null=True)
     in_catalog = models.BooleanField(default=False, verbose_name="Show in catalog?")
 
     def __str__(self):
         return self.title
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=70, help_text="Use short name if possible", unique=True)
+
+    def __str__(self):
+        return self.name
