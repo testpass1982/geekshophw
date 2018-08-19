4953853d10046adc7263fd301d3cf77a1220861d
Anatoly Popov <popov.anatoly@gmail.com>
2018-08-19 10:59:17 +0300

    Added model of product

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    manufacturer = models.CharField(max_length=70)
+    size = models.IntegerField(default=0)
+    date_added = models.DateTimeField(blank=True, null=True)
+    in_catalog = models.BooleanField(default=False, verbose_name="Show in catalog?")
+
+    def __str__(self):
+        return self.title
