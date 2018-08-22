7309f5121286387405035d41f7f42963f271e5bc
Anatoliy Popov <popov@naks.ru>
2018-08-22 14:43:48 +0300

    added models product and manufacturer

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 import mainapp.views as mainapp
 
 urlpatterns = [
@@ -29,3 +31,5 @@ urlpatterns = [
     path('login/', mainapp.login, name='login'),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
